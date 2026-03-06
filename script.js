69a6339e201585661a3f9b87f5ec96c719331ed7
bhuvanambhore <bhuvanmambhore@gmail.com>
2026-03-06 22:47:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const search = document.getElementById("search")
 
 async function addVideo(){
 
-const url = document.getElementById("videoLink").value
+let url = document.getElementById("videoLink").value
 
 if(!url) return
 
@@ -16,7 +16,8 @@ let data = await res.json()
 
 videos.push({
 title:data.title,
-url:url
+url:url,
+thumb:data.thumbnail_url
 })
 
 localStorage.setItem("videos",JSON.stringify(videos))
@@ -27,21 +28,48 @@ showVideos(videos)
 
 }
 
+function playVideo(id,videoId){
+
+document.getElementById(id).innerHTML =
+`<iframe src="https://www.youtube.com/embed/${videoId}" allowfullscreen></iframe>`
+
+}
+
+function deleteVideo(index){
+
+videos.splice(index,1)
+
+localStorage.setItem("videos",JSON.stringify(videos))
+
+showVideos(videos)
+
+}
+
 function showVideos(list){
 
 container.innerHTML=""
 
-list.forEach(v=>{
+list.forEach((v,i)=>{
 
-let id = v.url.split("v=")[1]
+let videoId = v.url.split("v=")[1]
 
-container.innerHTML += `
+container.innerHTML +=
+`
 <div class="video">
+
 <h3>${v.title}</h3>
-<iframe src="https://www.youtube.com/embed/${id}" allowfullscreen></iframe>
+
+<div id="player${i}">
+<img src="${v.thumb}" class="thumbnail"
+onclick="playVideo('player${i}','${videoId}')">
+</div>
+
+<br>
+
+<button onclick="deleteVideo(${i})">Delete</button>
+
 </div>
 `
-
 })
 
 }
