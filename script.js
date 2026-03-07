3f6b8b94d585fc6b6ee1dc7278b1b3e110bd8da5
bhuvanambhore <bhuvanmambhore@gmail.com>
2026-03-07 22:58:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-const repo="bhuvanambhore/mytube"
-const container=document.getElementById("videoGrid")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-async function loadVideos(){
+const firebaseConfig = {
+apiKey: "AIzaSyC6K2g9iMqZkQg8QJV1dU_dqpQXjmcqfPY",
+authDomain: "mytube-pro-8574b.firebaseapp.com",
+projectId: "mytube-pro-8574b",
+storageBucket: "mytube-pro-8574b.firebasestorage.app",
+messagingSenderId: "257345211479",
+appId: "1:257345211479:web:de0b2e790a4d4dc4642712"
+};
 
-const res=await fetch(`https://api.github.com/repos/${repo}/issues`)
-const issues=await res.json()
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-container.innerHTML=""
+const videosDiv=document.getElementById("videos")
 
-issues.forEach(issue=>{
+window.addVideo = async function(){
 
-const url=issue.body.match(/https?:\/\/[^\s]+/)
+let link=document.getElementById("videoLink").value
 
-if(!url) return
+if(!link) return
 
-const video=url[0]
-const id=video.split("v=")[1]
+let id=link.split("v=")[1]?.split("&")[0]||link.split("/").pop()
 
-const thumb=`https://img.youtube.com/vi/${id}/hqdefault.jpg`
+await addDoc(collection(db,"videos"),{
+url:link,
+created:Date.now()
+})
 
-container.innerHTML+=`
+document.getElementById("videoLink").value=""
 
-<div class="videoCard" onclick="playVideo('${video}')">
+}
 
-<img src="${thumb}">
+function play(url){
 
-<div class="videoTitle">
-${issue.title}
+let id=url.split("v=")[1]?.split("&")[0]||url.split("/").pop()
+
+document.getElementById("player").innerHTML=
+`<iframe src="https://www.youtube.com/embed/${id}" allowfullscreen></iframe>`
+
+}
+
+const q=query(collection(db,"videos"),orderBy("created","desc"))
+
+onSnapshot(q,(snapshot)=>{
+
+videosDiv.innerHTML=""
+
+snapshot.forEach(doc=>{
+
+let v=doc.data()
+let id=v.url.split("v=")[1]?.split("&")[0]||v.url.split("/").pop()
+
+let thumb=`https://img.youtube.com/vi/${id}/hqdefault.jpg`
+
+videosDiv.innerHTML+=`
+<div class="video">
+<img src="${thumb}" onclick="play('${v.url}')">
 </div>
-
-</div>
-
 `
 
 })
 
-}
-
-loadVideos()
+})
